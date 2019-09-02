fd03b74040a6373e6848dd7edb9d6db17e5d1729
Mariam <Mariamhh98@hotmail.com>
2019-09-02 15:42:35 +0300

    Manuel and Mariam

--- flights/views.py
+++ flights/views.py
@@ -1,1 +1,14 @@
+from rest_framework.generics import ListAPIView
+from .serializers import FlightListSerializer, BookingListSerializer
 from .models import Flight, Booking
+from django.utils import timezone
+
+class HotelListView (ListAPIView):
+	queryset = Flight.objects.all()
+	serializer_class = FlightListSerializer
+
+class BookingListView (ListAPIView):
+	queryset = Booking.objects.filter(date__gte = timezone.now())
+	serializer_class = BookingListSerializer
+
+
